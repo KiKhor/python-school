edb2b3047f148e27c2173afb1da0b6dd35bcbb00
Kikhor <horoshev.k.s@rbiu.ru>
2019-05-22 16:11:08 +0500

    Update DZ15.py

--- DZ15.py
+++ DZ15.py
@@ -4,11 +4,22 @@ import pprint
 conn = sqlite3.connect('Chinook_Sqlite.sqlite')
 cursor = conn.cursor()
 
-request_text ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*)
+request_text1 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*)
                 FROM Customer
                 GROUP BY City
                 HAVING COUNT(*) > 1'''
-cursor.execute(request_text)
+
+request_text2 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*)
+                FROM Customer
+                GROUP BY City
+                HAVING COUNT(*) > 1'''
+
+request_text3 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*)
+                FROM Customer
+                GROUP BY City
+                HAVING COUNT(*) > 1'''
+
+cursor.execute(request_text1)
 results = cursor.fetchall()
 pprint.pprint(results)
 
