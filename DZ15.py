bdaad062dd7296ad20c2fcd86863b888d421af14
KiKhor <horoshev.k.s@rbiu.ru>
2019-05-23 01:37:05 +0500

    Update DZ15.py

--- DZ15.py
+++ DZ15.py
@@ -11,19 +11,22 @@ request_text1 ='''SELECT DISTINCT C.FirstName||' '||C.LastName AS FullName, C.Ph
                             GROUP BY City
                             HAVING CountInCity > 1) AS N ON N.City = C.City
                 LEFT JOIN Invoice AS I ON C.CustomerId = I.CustomerId
-                WHERE I.Total > 0'''
+                WHERE I.Total > 0
+                ORDER BY FullName'''
 
-request_text2 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*) AS CountInCity
-                FROM Customer
-                GROUP BY City
-                HAVING CountInCity > 1'''
+request_text2 ='''SELECT BillingCity, SUM(Total)
+                FROM Invoice
+                GROUP BY BillingCity
+                HAVING SUM(Total)
+                ORDER BY SUM(Total) desc
+                LIMIT 3'''
 
 request_text3 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*)
                 FROM Customer
                 GROUP BY City
                 HAVING COUNT(*) > 1'''
 
-cursor.execute(request_text1)
+cursor.execute(request_text2)
 results = cursor.fetchall()
 pprint.pprint(results)
 
