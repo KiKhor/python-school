4a9a60d58815cc92ebe761625c37e7f6265ace5f
Kikhor <horoshev.k.s@rbiu.ru>
2019-05-22 17:08:14 +0500

    Update DZ15.py

--- DZ15.py
+++ DZ15.py
@@ -4,12 +4,14 @@ import pprint
 conn = sqlite3.connect('Chinook_Sqlite.sqlite')
 cursor = conn.cursor()
 
-request_text1 ='''SELECT C.FirstName||' '||C.LastName AS FullName, C.City, N.CountInCity
+request_text1 ='''SELECT DISTINCT C.FirstName||' '||C.LastName AS FullName, C.Phone
                 FROM Customer AS C
                 INNER JOIN (SELECT FirstName||' '||LastName AS FullName, City, COUNT(*) AS CountInCity
                             FROM Customer
                             GROUP BY City
-                            HAVING CountInCity > 1) AS N ON N.City = C.City'''
+                            HAVING CountInCity > 1) AS N ON N.City = C.City
+                LEFT JOIN Invoice AS I ON C.CustomerId = I.CustomerId
+                WHERE I.Total > 0'''
 
 request_text2 ='''SELECT FirstName||' '||LastName AS FullName, City, COUNT(*) AS CountInCity
                 FROM Customer
